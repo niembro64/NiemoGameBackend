d1b2a9250a6639578f6fedc165a94402f3171118
Eric Niemeyer <niemeyer.eric@gmail.com>
2023-09-05 00:33:29 -0400

    working

---
+++ server.ts
@@ -0,0 +1,42 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const httpServer = http.createServer(app);
+const io = new Server(httpServer);
+
+const deviceCoordinates: { [socketId: string]: { x: number; y: number } } = {};
+
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+
+  socket.on('send-coordinates', (data) => {
+    console.log('Received coordinates:', data);
+    deviceCoordinates[socket.id] = data;
+
+    // Broadcast the entire set of coordinates to all clients
+    io.emit('receive-coordinates', deviceCoordinates);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected', socket.id);
+
+    // Remove this device's coordinates
+    delete deviceCoordinates[socket.id];
+
+    // Broadcast the updated set of coordinates to all clients
+    io.emit('receive-coordinates', deviceCoordinates);
+  });
+
+  socket.on('connect_error', (err) => {
+    console.log('Connection Error:', err);
+  });
+
+  socket.on('error', (err) => {
+    console.log('Error:', err);
+  });
+});
+httpServer.listen(3000, () => {
+  console.log('listening on *:3000');
+});
