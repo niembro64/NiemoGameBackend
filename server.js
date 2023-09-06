980ee1168a52cc982767e380c5968566957720d0
Eric Niemeyer <niemeyer.eric@gmail.com>
2023-09-06 01:01:02 -0400

    const express

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-import express from 'express';
+// import express from 'express';
+
 import http from 'http';
 import { Server } from 'socket.io';
 
+const express = require('express');
 const app = express();
 const httpServer = http.createServer(app);
 const io = new Server(httpServer);
