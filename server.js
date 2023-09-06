dcb67c3690beed065757a78bfededff6786397de
Eric Niemeyer <niemeyer.eric@gmail.com>
2023-09-06 01:42:50 -0400

    ping pong

--- server.js
+++ server.js
@@ -2,14 +2,10 @@ import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
 
-// const http = require('http');
-
-// const express = require('express');
 const app = express();
 const httpServer = http.createServer(app);
 const io = new Server(httpServer);
 
-// const deviceCoordinates: { [socketId: string]: { x: number; y: number } } = {};
 const deviceCoordinates = {};
 
 io.on('connection', (socket) => {
@@ -17,7 +13,6 @@ io.on('connection', (socket) => {
 
   socket.on('register-device', (deviceId) => {
     console.log('Device ID:', deviceId);
-    // Store the device ID, associate it with the socket.id if needed
     deviceCoordinates[deviceId] = {
       x: 0,
       y: 0,
@@ -26,18 +21,12 @@ io.on('connection', (socket) => {
 
   socket.on('send-coordinates', (data) => {
     deviceCoordinates[data.deviceId] = data.positions;
-
     console.log('Broadcasting coordinates:', deviceCoordinates);
     io.emit('receive-coordinates', deviceCoordinates);
   });
 
   socket.on('disconnect', () => {
     console.log('user disconnected', socket.id);
-
-    // Remove this device's coordinates
-    // delete deviceCoordinates[socket.id];
-
-    // Broadcast the updated set of coordinates to all clients
     io.emit('receive-coordinates', deviceCoordinates);
   });
 
@@ -48,11 +37,15 @@ io.on('connection', (socket) => {
   socket.on('error', (err) => {
     console.log('Error:', err);
   });
+
+  // Log when the server receives a ping. This is for diagnostic purposes.
+  socket.on('ping', () => {
+    
+    socket.emit('pong');
+    console.log('Received ping from', socket.id);
+  });
 });
 
-// httpServer.listen(3000, () => {
-//   console.log('listening on *:3000');
-// });
 httpServer.listen(3000, '0.0.0.0', () => {
   console.log('listening on *:3000');
 });
